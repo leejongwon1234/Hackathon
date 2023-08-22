eaf0b8d4e5de4df22ecfdc5a4f5ddc49406fe5e0
Jongwon_2048 <123047859+leejongwon1234@users.noreply.github.com>
2023-08-22 20:40:06 +0900

    Update 데이터_상관관계.py

--- kt통신망_AI_해커톤/데이터_상관관계.py
+++ kt통신망_AI_해커톤/데이터_상관관계.py
@@ -1,8 +1,8 @@
 import pandas as pd
 
-new_train_data = pd.read_csv('Q1_train.csv')
+new_train_data = pd.read_csv('읽을 데이터 이름')
 
-correlation_train = new_train_data.corr()['uenomax'].drop('uenomax')
+correlation_train = new_train_data.corr()['상관관계를 관찰하고 싶은 열이름'].drop('상관관계를 관찰하고 싶은 열이름')
 
 # 내림차순으로 정렬
 correlation_train = correlation_train.sort_values(ascending=False)
