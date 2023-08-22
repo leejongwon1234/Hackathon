cdd7956f242759e8c732f668c9c9b139a58a008c
Jongwon_2048 <123047859+leejongwon1234@users.noreply.github.com>
2023-08-22 20:39:05 +0900

    Update 데이터_시각화.py

--- kt통신망_AI_해커톤/데이터_시각화.py
+++ kt통신망_AI_해커톤/데이터_시각화.py
@@ -2,12 +2,12 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 # Load the train data
-train_data = pd.read_csv('Q1_train.csv')
+train_data = pd.read_csv('데이터 시각화할 CSV파일 이름')
 # Convert the 'datetime' column to datetime format for the train data
 train_data['datetime'] = pd.to_datetime(train_data['datetime'])
 
 # Load the test data
-test_data = pd.read_csv('Q1_test.csv')
+test_data = pd.read_csv('데이터 시각화할 CSV파일 이름 2')
 # Convert the 'datetime' column to datetime format for the test data
 test_data['datetime'] = pd.to_datetime(test_data['datetime'])
 
@@ -19,11 +19,11 @@ distinct_colors = ['b', 'g', 'r', 'c', 'm', 'y', 'k', '#FFA500']
 
 # Resample and plot function
 def resample_and_plot(column_name, data_to_use):
-    daily_avg = data_to_use.groupby('ru_id').resample('D', on='datetime').mean().reset_index()
+    daily_avg = data_to_use.groupby('그룹화할 열 이름').resample('D', on='datetime').mean().reset_index()
     
     plt.figure(figsize=(10, 10))
-    for idx, ru_id in enumerate(daily_avg['ru_id'].unique()):
-        subset = daily_avg[daily_avg['ru_id'] == ru_id]
+    for idx, ru_id in enumerate(daily_avg['그룹화할 열 이름'].unique()):
+        subset = daily_avg[daily_avg['그룹화할 열 이름'] == ru_id]
         plt.plot(subset['datetime'], subset[column_name], label=ru_id, color=distinct_colors[idx % len(distinct_colors)])
     
     plt.title(f'Daily Average {column_name.upper()} by RU_ID')
@@ -34,9 +34,9 @@ def resample_and_plot(column_name, data_to_use):
     plt.tight_layout()
     plt.show()
 
-resample_and_plot('uenomax', train_data)
+resample_and_plot('데이터 시각화할 열 이름', train_data)
 
-for column_name in ['erabaddatt', 'erabaddsucc', 'endcmodbysgnbatt', 'endcmodbysgnbsucc', 'nummsg3', 'endcaddatt', 'endcaddsucc']:
+for column_name in ['데이터 시각화할 열 이름 리스트']:
 
     resample_and_plot(column_name, combined_data)
 
